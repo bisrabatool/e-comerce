4f3c031044b8da507cf352fb56ec673d5cbb4ba0
Bisra Batool <bisrabatool345@gmail.com>
2024-03-12 09:22:18 -0700

    update the product list

--- src/productlist.js
+++ src/productlist.js
@@ -31,22 +31,6 @@ const ProductList = () => {
 };
 
 export default ProductList;
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import ProductItem from './productitem';
+// 
 
-// const ProductList = ({ products }) => {
-//   return (
-//     <div className="product-list">
-//       <h2>Product List</h2>
-//       {products.map((product) => (
-//         <Link to={`/product/${product.id}`} key={product.id}>
-//           <ProductItem {...product} />
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// };
-
-export default ProductList;
 
